065fee5faa09bbc4b96cb1effef0eb099e5b0e48
lienpiao <511169817@qq.com>
2021-01-30 10:43:24 +0800

    CLRStats查看应用状态和统计 /clr

---
+++ Cappuccino.Web/Startup.cs
@@ -0,0 +1,20 @@
+﻿using CLRStats;
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(Cappuccino.Web.Startup))]
+namespace Cappuccino.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // 有关如何配置应用程序的详细信息，请访问 https://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseCLRStatsDashboard();
+        }
+    }
+}
